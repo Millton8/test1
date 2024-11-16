e3f713003b25fd4469e4c925542161429f910914
Millton8 <ienri@mail.ru>
2024-11-16 15:57:12 +0700

    add25

--- Program.cs
+++ Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
+        while (true)
+        {
         Console.WriteLine("Hello, World!10");
+        Thread.Sleep(10000);
+        }
     }
 }
