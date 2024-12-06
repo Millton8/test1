aebe618bb00ce06e928ac4f3ac2fc94266842145
Milltov8 <ienri1@mail.ru>
2024-12-06 08:31:49 +0700

    add29

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ class Program
     {
         while (true)
         {
-        Console.WriteLine("Hello, World!13");
+        Console.WriteLine("Hello, World!14");
         Thread.Sleep(10000);
         }
     }
